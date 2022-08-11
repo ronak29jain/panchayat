863ccf48aac0e2f9aa3e5e2964b0e4b258a86d1a
Ronak Jain <ronak290jain@gmail.com>
2022-08-11 16:48:07 +0530

    Added Weather Search in widgets (defult Gwalior)

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,88 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Widgets.css';
+import axios from 'axios';
 
 function Widgets() {
+
+  const [data, setData] = useState({})
+  const [defdata, setDefdata] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=85407d870e46afd279969cee64788f46`
+  
+  const searchLocation = (event) => {
+    if (event.key === "Enter"){
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      // setTemp(data.main.temp - 273)
+      setLocation('')
+    }
+  }
+  
+  useEffect(() => {
+    const defweatherdata = () => {
+      const defaulturl = 'https://api.openweathermap.org/data/2.5/weather?q=Gwalior&units=metric&appid=85407d870e46afd279969cee64788f46'
+      axios.get(defaulturl).then((response) => {
+        setDefdata(response.data);
+        console.log("this is test", response.data)
+      })
+    }
+    defweatherdata();
+  },[])
+
   return (
     <div className='widgets'>
-      <h1>Widgets</h1>
+      {/* <div className="searchbox">
+        <input 
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text"
+        />
+      </div> */}
+      <div className="container">
+        <div className="searchbox">
+          <input 
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text"
+          />
+        </div>
+        <div className="top">
+          <div className="location">
+            {defdata.name ? (data.name ? <p>{data.name}</p> : <p>{defdata.name}</p>) : null}
+          </div>
+          <div className="temp">
+            {/* {data.main ? <h1>{number(data.main.temp)-273}-273°C</h1> : <h1>25°C</h1>} */}
+            {defdata.main ?  (data.main ? <h1>{data.main.temp}°C</h1> : <h1>{defdata.main.temp}°C</h1>) : null}
+          </div>
+          <div className="discription">
+            {defdata.weather ?  (data.weather ? <p>{data.weather[0].main}</p> : <p>{defdata.weather[0].main}</p>) : null}
+            {/* {data.weather ? <p>{data.weather[0].main}</p> : <p>Clouds</p>} */}
+          </div>
+        </div>
+        <div className="bottom">
+          {/* <div className="feels">
+            <p className='bold'>30°C</p>
+            <p>Feels Like</p>
+          </div> */}
+          <div className="humidity">
+            {defdata.main ? (data.main ? <p className='bold'>{data.main.humidity}%</p> : <p className='bold'>{defdata.main.humidity}%</p>) : null}
+            {/* {data.main ? <p className='bold'>{data.main.humidity}%</p> : <p className='bold'>40%</p>} */}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            {defdata.wind ? (data.wind ? <p className='bold'>{data.wind.speed} MPH</p> : <p className='bold'>{defdata.wind.speed} MPH</p>) : null}
+            {/* {data.wind ? <p className='bold'>{data.wind.speed} MPH</p> : <p className='bold'>15 MPH</p>} */}
+            <p>Winds</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
